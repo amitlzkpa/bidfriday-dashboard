1ba275d22cc77a1ab9e81045dbdfd0a1f5d58317
amitlzkpa <amit.lzkpa@gmail.com>
2020-07-23 05:22:35 -0500

    add dummy overview table

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
+
+    
     <p>
       Hi {{ name }}
     </p>
 
+
+    <h4>New Request</h4>
     <input type="text" v-model="newBoardName" />
-    <button @click="onNewBoardClk">New Request</button>
+    <button @click="onNewBoardClk">Create</button>
+
+
+    <br /><br />
+
+
+    <button @click="refresh">Refresh</button>
+
+    <h4>Active Requests</h4>
+
+    <table style="width:100%">
+
+      <tr>
+        <th>Name</th>
+        <th>Total Budget</th>
+        <th>Best Budget</th>
+        <th>Total Items</th>
+        <th>Items Ordered</th>
+        <th>Items Delivered</th>
+        <th>Bid Received</th>
+        <th>Vendor Pool</th>
+      </tr>
+
+      <tr v-for="row in rows" :key="row.name">
+        <td>{{ row.name }}</td>
+        <td>$42</td>
+        <td>$32</td>
+        <td>32</td>
+        <td>12</td>
+        <td>1</td>
+        <td>6</td>
+        <td>+</td>
+      </tr>
+      
+    </table>
+
 
     <br /><br />
 
@@ -20,7 +59,8 @@ export default {
   data () {
     return {
       name: null,
-      newBoardName: "My new request"
+      newBoardName: "My new request",
+      rows: []
     };
   },
   async mounted () {
@@ -31,8 +71,20 @@ export default {
       ctx = res.data;
     });
 
+    this.refresh();
+
   },
   methods: {
+    async refresh() {
+      while(!ctx) await this.wait(200);
+
+      let boardId = ctx.boardId;
+      let queryStr = `query { boards (ids: ${boardId}) { name items { name id } } }`;
+      console.log(queryStr);
+      let res = await this.monday.api(queryStr);
+      console.log(res.data);
+      this.rows = res.data.boards[0].items;
+    },
     async onNewBoardClk() {
       if(!this.newBoardName || this.newBoardName === "") return;
       while(!ctx) await this.wait(200);
